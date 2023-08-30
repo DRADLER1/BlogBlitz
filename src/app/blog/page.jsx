3bf5e87a06a17d3f9b5b84310fc5b46157221494
Shubham <dradlershubham@gmail.com>
2023-08-30 11:23:06 +0530

    try deploy 6

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -8,7 +8,7 @@ import Image from "next/image";
  const getData = async() => {
   const res = await fetch(`http://localhost:3000/api/posts`, {
     cache: "no-store",
-);
+  });
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
